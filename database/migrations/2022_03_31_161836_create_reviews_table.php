c1b376fa90a752a35c7789d7a31f52462cf9c59a
toyou1 <toyou4052@gmail.com>
2022-04-03 16:41:26 +0900

    コミットコメントです。

---
+++ database/migrations/2022_03_31_161836_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title',50);
+            $table->string('body',200);
+            $table->bigIncrements('user_id');
+            $table->bigIncrements('post_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
